6056218b35fc10ef3dd3f8571f7fc83468f4a63c
CalebRHaynes <44538013+CalebRHaynes@users.noreply.github.com>
2020-01-14 17:40:59 -0500

    no mean centering, checking normality of k value

--- accom_analysis.R
+++ accom_analysis.R
@@ -12,20 +12,15 @@ library("QuantPsyc")
 df <- read_excel("accom_clean.xlsx")
 attach(df)
 
-#mean center k, normality tests-----------------------------------------------------------
+#normality tests-----------------------------------------------------------
 
 #initial normality tests 
 shapiro.test(discount_rate_k)
 shapiro.test(percent_delayed_choices)
 
-#mean center k
-
-discount_rate_k_cent <- meanCenter(discount_rate_k)
-
 #log transform discount rate k 
-
-discount_rate_k_cent_transformed <- log10(discount_rate_k_cent + 1)
-shapiro.test(discount_rate_k_cent_transformed)
+discount_rate_k_transformed <- log10(discount_rate_k + 1)
+shapiro.test(discount_rate_k_transformed)
 
 # plotting ----------------------------------------------------------------
 
@@ -36,10 +31,10 @@ polygon(disc_k, col="lightgray", border="gray")
 rug(discount_rate_k_transformed, col="red")
 
 #discount rate k normality log transformed 
-disc_k_c_t <- density(discount_rate_k_cent_transformed)
-plot(disc_k_c_t, type="n", main="discount rate k (transformed and centered)")
-polygon(disc_k_c_t, col="lightgray", border="gray")
-rug(discount_rate_k_cent_transformed, col="red")
+disc_k_t <- density(discount_rate_k_transformed)
+plot(disc_k_t, type="n", main="discount rate k (transformed)")
+polygon(disc_k_t, col="lightgray", border="gray")
+rug(discount_rate_k_transformed, col="red")
 
 #percent of choices delayed normality
 perent_delayed <- density(percent_delayed_choices)
