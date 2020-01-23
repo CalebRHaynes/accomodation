06191686a5ec853a75d6d3eb2d0855ab82e142ac
CalebRHaynes <44538013+CalebRHaynes@users.noreply.github.com>
2020-01-23 10:32:40 -0500

    updating dataset, waiting on more survey data then will do correlations on subset

--- accom_analysis.R
+++ accom_analysis.R
@@ -10,31 +10,26 @@ library("dplyr")
 library("QuantPsyc")
 #import data---------------------------------------------------------------
 df <- read_excel("accom_clean.xlsx")
-attach(df)
-
+#create subset of data to exclude subjects with weird output that is yet to be debugged 
+df_subset <- subset(df, df$exclude == 0)
+attach(df_subset)
+k <- as.numeric(discount_rate_k)
+percent_delayed_choices <- as.numeric(percent_delayed_choices)
 #normality tests-----------------------------------------------------------
 
 #initial normality tests 
-shapiro.test(discount_rate_k)
+shapiro.test(k)
 shapiro.test(percent_delayed_choices)
-
 #log transform discount rate k 
-discount_rate_k_transformed <- log10(discount_rate_k)
-shapiro.test(discount_rate_k_transformed)
+k_transformed <- log10(k)
+shapiro.test(k_transformed)
 
 # plotting ----------------------------------------------------------------
-
-#discount rate k normality
-disc_k <- density(discount_rate_k)
-plot(disc_k, type="n", main="discount rate k")
-polygon(disc_k, col="lightgray", border="gray")
-rug(discount_rate_k_transformed, col="red")
-
 #discount rate k normality log transformed 
-disc_k_t <- density(discount_rate_k_transformed)
+disc_k_t <- density(k_transformed)
 plot(disc_k_t, type="n", main="discount rate k (transformed)")
 polygon(disc_k_t, col="lightgray", border="gray")
-rug(discount_rate_k_transformed, col="red")
+rug(k_transformed, col="red")
 
 #percent of choices delayed normality
 perent_delayed <- density(percent_delayed_choices)
@@ -42,9 +37,5 @@ plot(perent_delayed, type="n", main="percent of choices for delayed")
 polygon(perent_delayed, col="lightgray", border="gray")
 rug(percent_delayed_choices, col="red")
 
-
-
-
-
-
+#correlations####
 
